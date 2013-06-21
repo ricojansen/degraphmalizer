05a52804c7d830588875f68fad0c1dccb1e1a207
Rico Jansen <rico.jansen@gmail.com>
2013-06-21 18:56:08 +0200

    Empty classes

---
+++ degraphmalizer-core/src/main/java/dgm/configuration/groovy/GroovyConfiguration.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2013 All rights reserved
+ * VPRO The Netherlands
+ */
+package dgm.configuration.groovy;
+
+/**
+ * User: rico
+ * Date: 21/06/2013
+ */
+public class GroovyConfiguration
+{
+}
---
+++ degraphmalizer-core/src/main/java/dgm/configuration/groovy/GroovyIndexConfig.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2013 All rights reserved
+ * VPRO The Netherlands
+ */
+package dgm.configuration.groovy;
+
+/**
+ * User: rico
+ * Date: 21/06/2013
+ */
+public class GroovyIndexConfig {
+}
